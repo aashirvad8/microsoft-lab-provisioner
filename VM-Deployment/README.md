9a48e83e7f208e683456627cba2c22eedaffacea
aashirvad8 <aashirvad8@gmail.com>
2025-10-23 17:40:00 +0530

    Delete VM-Deployment directory

--- VM-Deployment/README.md
+++
@@ -1,1 +0,0 @@
-# microsoft-lab-provisioner
--- VM-Deployment/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "vm_public_ip" {
-  description = "Public IP of the created Windows VM"
-  value       = azurerm_public_ip.lab_pip.ip_address
-}
-
-output "vm_name" {
-  value = azurerm_windows_virtual_machine.lab_vm.name
-}
